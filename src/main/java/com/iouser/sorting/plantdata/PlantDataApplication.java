bbf94ead77727c2781cec6dbeed66e5728c9ce7d
meraihan <sayedmahmudraihan@gmail.com>
2020-09-20 19:58:34 +0600

    initial structure

---
+++ src/main/java/com/iouser/sorting/plantdata/PlantDataApplication.java
@@ -0,0 +1,13 @@
+package com.iouser.sorting.plantdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iouser/sorting/plantdata/PlantDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iouser.sorting.plantdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
