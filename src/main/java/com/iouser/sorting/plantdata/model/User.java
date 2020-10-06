091e3053d0d265041f66c4055db118b261d101f8
meraihan <sayedmahmudraihan@gmail.com>
2020-10-07 01:13:53 +0600

    annotation changed

--- src/main/java/com/iouser/sorting/plantdata/model/User.java
+++ src/main/java/com/iouser/sorting/plantdata/model/User.java
@@ -14,19 +14,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @JoinColumn(name = "first_name")
+    @Column(name = "first_name")
     private String firstName;
-    @JoinColumn(name = "last_name")
+    @Column(name = "last_name")
     private String lastName;
-    @JoinColumn(name = "email")
+    @Column(name = "email")
     private String email;
-    @JoinColumn(name = "username")
+    @Column(name = "username")
     private String userName;
-    @JoinColumn(name = "password")
+    @Column(name = "password")
     private String password;
-    @JoinColumn(name = "role")
+    @Column(name = "role")
     private String role;
-    @JoinColumn(name = "created_at")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
